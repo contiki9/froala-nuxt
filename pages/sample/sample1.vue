2edd75e7e70face0f7422edd348c304003dabf21
平野正樹 <contiki9@gmail.com>
2019-06-09 18:50:17 +0900

    buld miss

--- pages/sample/sample1.vue
+++ pages/sample/sample1.vue
@@ -13,15 +13,15 @@
 </template>
 
 <script>
-    import CallToActionFdbBlock1 from '~components/froala-design-blocks/callToAction/FdbBlock1.vue'
-    import HeadersFdbBlock1 from '~components/froala-design-blocks/headers/FdbBlock1.vue'
-    import FeaturesFdbBlock26 from '~components/froala-design-blocks/features/FdbBlock26.vue'
-    import ContentsFdbBlock14 from '~components/froala-design-blocks/contents/FdbBlock14.vue'
-    import TestimonialsFdbBlock2 from '~components/froala-design-blocks/Testimonials/FdbBlock2.vue'
-    import TestimonialsFdbBlock4 from '~components/froala-design-blocks/Testimonials/FdbBlock4.vue'
-    import ContactsFdbBlock4 from '~components/froala-design-blocks/contacts/FdbBlock4.vue'
-    import TestimonialsFdbBlock1 from '~components/froala-design-blocks/testimonials/FdbBlock1.vue'
-    import FootersFdbBlock9 from '~components/froala-design-blocks/footers/FdbBlock9.vue'
+    import CallToActionFdbBlock1 from '~/components/froala-design-blocks/callToAction/FdbBlock1'
+    import HeadersFdbBlock1 from '~/components/froala-design-blocks/headers/FdbBlock1'
+    import FeaturesFdbBlock26 from '~/components/froala-design-blocks/features/FdbBlock26'
+    import ContentsFdbBlock14 from '~/components/froala-design-blocks/contents/FdbBlock14'
+    import TestimonialsFdbBlock2 from '~/components/froala-design-blocks/Testimonials/FdbBlock2'
+    import TestimonialsFdbBlock4 from '~/components/froala-design-blocks/Testimonials/FdbBlock4'
+    import ContactsFdbBlock4 from '~/components/froala-design-blocks/contacts/FdbBlock4'
+    import TestimonialsFdbBlock1 from '~/components/froala-design-blocks/testimonials/FdbBlock1'
+    import FootersFdbBlock9 from '~/components/froala-design-blocks/footers/FdbBlock9'
 
     export default {
         components: {
